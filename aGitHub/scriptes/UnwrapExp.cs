238c6da094810b260c1b724bb07dc1a5280e2c69
Stas <jagdtigr124@gmail.com>
2022-03-15 20:53:27 +0300

    Добавлена сцена и новый чужая функция
    Signed-off-by: Stas <jagdtigr124@gmail.com>

--- aGitHub/scriptes/UnwrapExp.cs
+++ aGitHub/scriptes/UnwrapExp.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class UnwrapExp : MonoBehaviour
     {
     public Mesh mesh;
-    public Material mat;
+    public Material mat, mirrorMat;
     public RawImage screen;
 
     public RenderTexture courrTex, resTex;
@@ -21,8 +21,7 @@ public class UnwrapExp : MonoBehaviour
         {
         //resTex = new RenderTexture(1024, 1024, 32);
         //resTex.Create();
-        screen.texture = resTex;
-
+        screen.texture = courrTex;
         buffer = new CommandBuffer();
         //Camera.main.AddCommandBuffer(CameraEvent.AfterFinalPass, buffer);
         }
@@ -38,28 +37,35 @@ public class UnwrapExp : MonoBehaviour
         buffer.DrawRenderer(dend, dend.material);
         Graphics.ExecuteCommandBuffer(buffer);
         */
-        objectMatrix = Matrix4x4.TRS(meshPosition, Quaternion.identity, Vector3.one * 0.3f);
+        //objectMatrix = Matrix4x4.TRS(meshPosition, Quaternion.identity, Vector3.one * 0.3f);
         
         Blit();
         }
 
     void Blit()
         {
+        Vector3 minCorn = mesh.bounds.min;
+        Vector3 maxCorn = mesh.bounds.max;
+        Vector3 delta = maxCorn - minCorn;
+        maxCorn -= minCorn;
+        float scale = Mathf.Max(maxCorn.x, maxCorn.y, maxCorn.z);
+        Vector3 bias = minCorn;
+
         // Create an orthographic matrix (for 2D rendering)
         // You can otherwise use Matrix4x4.Perspective()
         Matrix4x4 projectionMatrix = Matrix4x4.Ortho(0, 1, 0, 1, 0.1f, 100);
 
         // This fixes flickering (by @guycalledfrank)
         // (because there's some switching back and forth between cameras, I don't fully understand)
-        if (Camera.current != null)
-            projectionMatrix *= Camera.current.worldToCameraMatrix.inverse;
 
         // Remember the current texture and set our own as "active".
         RenderTexture prevRT = RenderTexture.active;
-        RenderTexture.active = resTex;
+        RenderTexture.active = courrTex;
 
         // Set material as "active". Without this, Unity editor will freeze.
         mat.SetPass(0);
+        mat.SetFloat("scale", scale);
+        mat.SetVector("bias", new Vector4(bias.x, bias.y, bias.z, 0));
 
         // Push the projection matrix
         GL.PushMatrix();
@@ -73,6 +79,7 @@ public class UnwrapExp : MonoBehaviour
 
         // Draw the mesh!
         Graphics.DrawMeshNow(mesh, objectMatrix);
+        //Graphics.Blit(resTex, courrTex);
 
         // Pop the projection matrix to set it back to the previous one
         GL.PopMatrix();
--- aGitHub/shaders/UnwrapShader.shader
+++ aGitHub/shaders/UnwrapShader.shader
@@ -1,13 +1,17 @@
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
 Shader "Unlit/UnwrapShader"
     {
     Properties
         {
         _MainTex ("Texture", 2D) = "white" {}
+        _ScaleParam("Scale_of_center", Range(0.0, 1.0)) = 0.5
         }
     SubShader
         {
         Tags { "RenderType"="Opaque" }
         LOD 100
+        Cull off
 
         Pass
             {
@@ -30,27 +34,32 @@ Shader "Unlit/UnwrapShader"
                 float2 uv : TEXCOORD0;
                 UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
+                float4 color  : COLOR_0;
                 };
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
+            float scale, _ScaleParam;
+            float4 bias;
 
             v2f vert (appdata v)
                 {
                 v2f o;
-                o.vertex = float4(v.uv.x, v.uv.y, 0.0f, 0.0f);
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                //UNITY_TRANSFER_FOG(o,o.vertex);
+                //v.vertex = float4(v.uv.xy * 2 - float2(1, 1), 0.0, 1.0);
+                o.vertex = float4(  ( float2(1, 1) - v.uv.xy * 2), 0.0, 1.0);
+                o.vertex.x = -o.vertex.x;
+                o.color = (v.vertex - bias) / scale;
                 return o;
                 }
 
             fixed4 frag (v2f i) : SV_Target
                 {
+                
                 // sample the texture
                 fixed4 col = tex2D(_MainTex, i.uv);
                 // apply fog
                 UNITY_APPLY_FOG(i.fogCoord, col);
-                return col;
+                return i.color;
                 }
             ENDCG
             }
